499ac176ae25432947a1407b6f2d3560a4439de7
thinktank01 <felixayot@gmail.com>
2023-10-26 12:57:35 +0300

    Update models/engine/file_storage.py with get() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,13 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID,
+           or None if not found.
+        """
+        if cls and id:
+            one_obj = "{}.{}".format(cls, id)
+            all_obj = self.all(cls)
+            return all_obj.get(one_obj)
+        return None
