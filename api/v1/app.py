b7537d088fb21307541a05b0beac86610abc30f6
thinktank01 <felixayot@gmail.com>
2023-10-27 00:31:58 +0300

    Upload task 5(Handle page not found status code=404)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Starts Flask web application."""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 import os
 from models import storage
 from api.v1.views import app_views
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -15,6 +16,13 @@ port = os.getenv("HBNB_API_PORT", 5000)
 def teardown(exc):
     """Removes the current SQLAlchemy session."""
     storage.close()
+
+
+@app.errorhandler(404)
+def status_code_404(exception):
+    """Returns a JSON-formatted 404 status code response."""
+    code = exception.__str__().split()[0]
+    return make_response(jsonify({"error": "Not found"}), code)
 
 
 if __name__ == "__main__":
