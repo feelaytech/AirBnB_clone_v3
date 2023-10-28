6fab336e42d0fee28b879ec21dd50818a2c1579f
thinktank01 <felixayot@gmail.com>
2023-10-28 10:45:33 +0300

    Upload task 12(Create a CORS instance to control HTTP Access Control)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Starts Flask web application."""
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 import os
 from models import storage
 from api.v1.views import app_views
@@ -10,6 +11,8 @@ app.register_blueprint(app_views)
 
 host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 port = os.getenv("HBNB_API_PORT", 5000)
+
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
