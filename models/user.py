2572404278df47f0da196b658ec32ec4dd988702
thinktank01 <felixayot@gmail.com>
2023-10-28 17:41:38 +0300

    Upload task 14(Update BaseModel & User models to remove and hash password when a new user is created.

--- models/user.py
+++ models/user.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ holds class User"""
-import models
+from models import storage_t
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
+import hashlib
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    if models.storage_t == 'db':
+    if storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
@@ -26,4 +27,15 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        if kwargs:
+            pwd = kwargs.pop("password", None)
+            if pwd:
+                User.__hash_password(self, pwd)
         super().__init__(*args, **kwargs)
+
+    def __hash_password(self, pwd):
+        """Constructor for Hash encrypting password attribute to MD5."""
+        hash = hashlib.md5()
+        hash.update(pwd.encode("utf-8"))
+        hashed_password = hash.hexdigest()
+        setattr(self, "password", hashed_password)
